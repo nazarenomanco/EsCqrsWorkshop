4e80a911723eb118371ffde5d6250a70c22a8e36
Nazareno Manco <nazareno.m@live.com>
2017-05-26 15:31:31 +0200

    Completata gestione del processo di notifica al delivery boy

---
+++ EsCqrsWorkshop.Domain.Handlers/NotifyDeliveryBoyHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EsCqrsWorkshop.Messages.Commands;
+using Jason.Handlers.Commands;
+
+namespace EsCqrsWorkshop.Domain.Handlers
+{
+    class NotifyDeliveryBoyHandler: AbstractCommandHandler<NotifyDeliveryBoy>
+    {
+        protected override object OnExecute(NotifyDeliveryBoy command)
+        {
+             // Handler Code HERE
+            return null;
+        }
+    }
+}
