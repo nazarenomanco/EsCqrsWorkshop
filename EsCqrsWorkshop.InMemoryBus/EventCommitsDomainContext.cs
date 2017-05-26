68caf36b6fc6f97a4c840f5be5b5f4143ff8b361
mauroservienti <mauro@topics.it>
2017-05-26 14:10:46 +0200

    Fix dependencies

---
+++ EsCqrsWorkshop.InMemoryBus/EventCommitsDomainContext.cs
@@ -0,0 +1,15 @@
+﻿using Radical.CQRS.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsCqrsWorkshop.InMemoryBus
+{
+    public class EventCommitsDomainContext : DomainContext
+    {
+        
+    }
+}
---
+++ EsCqrsWorkshop.InMemoryBus/Services/UnreliableServiceBus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsCqrsWorkshop.InMemoryBus.Services
+{
+    public class UnreliableServiceBus
+    {
+        public void StartPolling()
+        {
+
+        }
+    }
+}
